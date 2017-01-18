53478cbc7e9bc39a83c8cd2f7d4c4deb846f74c8
morgan.fromentin <morgan.fromentin@ezbeez.eu>
2017-01-18 16:43:27 +0100

    some cleaning

--- webhook.php
+++ webhook.php
@@ -8,13 +8,10 @@ const WEBHOOKURL = "https://discordapp.com/api/webhooks/270664905894526986/QxJiz
 
 // GitHub send data using POST method
 if ('POST' === $_SERVER['REQUEST_METHOD']) {
-    // get the POST Request Body
     $decodedJson = json_decode(file_get_contents('php://input'), true);
     $resolver    = new WebhookResolver();
     $event       = $resolver->resolve($decodedJson); // for ex, we get instance of PullRequestEvent
-    //file_put_contents('events.log', PHP_EOL. get_class($event)." - ".$event->getRepository()->getFullName()." : ".$event::name(), FILE_APPEND | LOCK_EX);
     if (($event instanceof Lpdigital\Github\EventType\PushEvent) || ($event instanceof Lpdigital\Github\EventType\PullRequestEvent)) {
-        //file_put_contents('events.log', PHP_EOL. get_class($event)." - ".$event->getRepository()->getFullName()." : ".$event::name(), FILE_APPEND | LOCK_EX);
         if ($event instanceof Lpdigital\Github\EventType\PushEvent) {
             $msg = "Un nouveau commit a été fait sur ".$event->getRepository()->getFullName()." : ";
             foreach ($event->commits as $commit) {
